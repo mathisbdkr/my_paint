624a3a4eaaa1d7f4994dc1e6e4f6432f9893121c
mathis <mathis@fedora-3.home>
2023-03-09 19:36:58 +0100

    segfault patch

--- src/bouton/save/src/ls.c
+++ src/bouton/save/src/ls.c
@@ -38,29 +38,36 @@ int nom_file(struct dirent *file, DIR *dir, struct save_t *save)
 
 void file_char(struct save_t *save, int res, int nb)
 {
-    save->file = malloc(sizeof(char) * (res + 3) + nb);
-    save->file[0] = '\0';
     DIR *dir = opendir("./");
+    if (!dir) {
+        return;
+    }
     struct dirent *file = readdir(dir);
-    save->file = my_strcat(save->file, file->d_name);
-    save->file = my_strcat(save->file, " ");
-    while (dir) {
-        if (nom_file(file, dir, save) == 84)
-            break;
+    save->file = malloc(sizeof(char) * (res + 3) + nb);
+    if (!save->file) {
+        closedir(dir);
+        return;
+    }
+    save->file[0] = '\0';
+    if (file->d_name[0] != '.') {
+        save->file = my_strcat(save->file, file->d_name);
+        save->file = my_strcat(save->file, " ");
+    }
+    while (nom_file(file, dir, save) != 84) {
     }
 }
 
 int ls_vide(struct save_t *save)
 {
     DIR *dir = opendir("./");
-    if (dir == NULL) {
+    if (!dir) {
         return 1;
     }
     struct dirent *file = readdir(dir);
     int len = 0;
     int res = my_strlen(file->d_name);
     int nb = 0;
-    while (dir) {
+    while ((file = readdir(dir))) {
         len = len_file(file, dir);
         if (len == -5)
             break;
@@ -68,4 +75,6 @@ int ls_vide(struct save_t *save)
         nb++;
     }
     file_char(save, res, nb);
+    closedir(dir);
+    return 0;
 }
